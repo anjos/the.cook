de5c733917287092fa451491154645b508525a8a
Andre Anjos <andre.dos.anjos@gmail.com>
2014-05-01 17:38:57 +0200

    Implement e-mail sending

--- the/cook/sendmail.py
+++ the/cook/sendmail.py
@@ -9,10 +9,36 @@ import os
 import sys
 import datetime
 import logging
-from .models import User, Lunch, Subscription, connect, format_date
+from .models import format_date, as_str, as_unicode
 from .menu import get_current_user, next_lunch
 
-def reminder(session, email):
+def sendmail(author, to, subject, contents, cc=None):
+  """Sends an e-mail using the Idiap SMTP server
+
+  author - A 'User' object that corresponds to the author of the message
+  to - A list, containing the parties that will receive the message
+  subject - The message subject
+  contents - The actual contents of the message, as a list w/o newlines
+  cc - An optional list of people to carbon-copy the message to
+  """
+
+  import smtplib
+  from email.mime.text import MIMEText
+
+  msg = MIMEText('\n'.join(contents))
+
+  msg['From'] = author.name_and_email()
+  msg['To']   = ', '.join(to)
+  msg['Subject'] = subject
+  if cc: msg['Cc'] = ', '.join(cc)
+
+  s = smtplib.SMTP('localhost')
+  recipients = to
+  if cc: recipients += cc
+  s.sendmail(author.name_and_email(), recipients, msg.as_string())
+  s.quit()
+
+def reminder(session, address, cc=None):
   "Sends a reminder for lunch subscription, with the menu"
 
   lunch = next_lunch(session)
@@ -23,7 +49,6 @@ def reminder(session, email):
 
   user = get_current_user(session)
   path = os.path.dirname(sys.argv[0])
-  session.close()
 
   tomorrow = datetime.date.today() + datetime.timedelta(days=1)
   if (lunch.date != tomorrow):
@@ -31,11 +56,12 @@ def reminder(session, email):
     return False
 
   message = [
-      "Hello, next %s, the Vatel Restaurant will organise" % format_date(lunch.date),
+      "Hello, next %s, the Vatel Restaurant will organise" % \
+          format_date(lunch.date),
       "lunch for all Idiapers that subscribed by today 18h00. The menu is:",
       "",
-      "Français: %s" % lunch.menu_french.encode('utf-8'),
-      "English: %s" % lunch.menu_english.encode('utf-8'),
+      "Français: %s" % as_str(lunch.menu_french),
+      "English: %s" % as_str(lunch.menu_english),
       "",
       "To subscribe to this lunch, execute the following command on a Linux",
       "workstation at Idiap:",
@@ -56,17 +82,19 @@ def reminder(session, email):
       "Yours faithfully, The Cook.",
       ]
 
-  if email is None:
-    print("From: %s <%s>" % (user.fullname(), user.email()))
-    print("To: Miscellaneous Varied Varieties <misc@idiap.ch>")
-    print("Subject: [food] [%s] %s" % (format_date(lunch.date), lunch.menu_french.encode('utf-8')))
+  subject = "[food] [%s] %s" % (format_date(lunch.date), lunch.menu_french)
+
+  if address is None:
+    print("From: %s" % user.name_and_email())
+    print("Subject: %s" % subject)
+    if cc: print("Cc: %s" % ", ".join(cc))
     print("")
     for l in message: print(l)
 
   else:
-    raise NotImplementedError("This bot cannot yet send messages")
+    sendmail(user, address, subject, message, cc)
 
-def report(session, email):
+def report(session, address, cc=None):
   "Sends a PDF report to the Vatel Restaurant"
 
   lunch = next_lunch(session)
@@ -81,14 +109,14 @@ def report(session, email):
   message = [
       "Menu proposé pour %s:" % format_date(lunch.date),
       "",
-      "\"%s\"" % lunch.menu_french.encode('utf-8'),
+      "\"%s\"" % as_str(lunch.menu_french),
       "",
       "Idiapers inscrits pour ce repas:",
       "",
       ]
 
   for s in lunch.subscriptions:
-    message.append("%s (%s) <%s>: %d (CHF %d.-)" % \
+    message.append("  - %s (%s) <%s>: %d personne(s) [CHF %d.-]" % \
         (s.user.fullname(), s.user.phone(), s.user.email(), s.persons,
           10*s.persons))
 
@@ -98,20 +126,20 @@ def report(session, email):
       "",
       "Merci de nous confirmer la bonne récéption de ce couriel,"
       "",
-      "%s <%s>" % (user.fullname(), user.email()),
+      "%s" % user.name_and_email(),
       ]
 
-  if email is None:
-    print("From: %s <%s>" % (user.fullname(), user.email()))
-    print("To: Réception - Hotel Vatel <info@hotelvatel.ch>")
-    print("Subject: Inscription pour le repas du %s" % format_date(lunch.date))
+  subject = "Inscription pour le repas du %s" % format_date(lunch.date)
+
+  if address is None:
+    print("From: %s" % user.name_and_email())
+    print("Subject: %s" % subject)
+    if cc: print("Cc: %s" % ", ".join(cc))
     print("")
     for l in message: print(l)
 
   else:
-    raise NotImplementedError("This bot cannot yet send messages")
-
-  session.close()
+    sendmail(user, address, subject, message, cc)
 
 def call(session, dry_run):
   "Sends a call for subscribes of the day lunch"
@@ -130,33 +158,52 @@ def call(session, dry_run):
       ""
       "This is a reminder that you have subscribed for the Idiap lunch today:",
       "",
-      "Français: %s" % lunch.menu_french.encode('utf-8'),
-      "English: %s" % lunch.menu_english.encode('utf-8'),
+      "Français: %s" % as_str(lunch.menu_french),
+      "English: %s" % as_str(lunch.menu_english),
       "",
-      "There are %d people subscribed in total" % len(lunch.subscriptions),
+      "There are %d people subscribed in total:" % lunch.total_subscribers(),
+      ""
+      ]
+
+  for s in lunch.subscriptions:
+    message2.append("  - %s: %d person(s)" % \
+        (s.user.name_and_email(), s.persons))
+
+  message = [
       "",
       "**Payment**: Payment for your lunch should be done before you eat",
       "your lunch. The Vatel Restaurant accepts that you pay just before",
       "eating, so you can pay when you go down for the lunch.",
+      "",
+      "Note: If you subscribed for more people than just yourself, you are",
+      "responsible for paying the total at the Vatel Restaurant for all the",
+      "people you vouched for. When you pay, demand and keep a receipt of",
+      "your payment. That is your sole proof of payment.",
+      "",
+      ]
+
+  message += [
       "",
       "Where: Downstairs, at the Idiap kitchen by default. You should procure",
       "your own cutlery (a fork and a knife) and beverage and bring that with",
       "you. If you are the first to arrive and the meal is not set on the hot",
       "plates, please go the Vatel Restaurant reception and ask them to serve",
       "the meal. All others thank you in advance.",
+      ""
+      "Time: The semi-official lunch time is 12h30",
       "",
       "Yours faithfully, The Cook.",
       ]
 
+  subject = "[food] [reminder] [%s] %s" % (format_date(lunch.date), as_str(lunch.menu_french))
+
   if dry_run:
-    print("From: %s <%s>" % (user.fullname(), user.email()))
-    print("To: %d users <...@idiap.ch>" % len(lunch.subscriptions))
-    print("Subject: [food] [reminder] [%s] %s" % (format_date(lunch.date), lunch.menu_french.encode('utf-8')))
+    print("From: %s" % user.name_and_email())
+    print("Subject: %s" % subject)
+    if cc: print("Cc: %s" % ", ".join(cc))
     print("")
     for l in message: print(l)
 
   else:
-    # TODO: Tell the person the number of subscribes and the total to pay
-    raise NotImplementedError("This bot cannot yet send messages")
-
-  session.close()
+    address = [k.user.name_and_email() for k in lunch.subscriptions]
+    sendmail(user, address, subject, message, cc)
